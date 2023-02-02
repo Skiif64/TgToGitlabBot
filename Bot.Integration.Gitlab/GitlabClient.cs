2e436816aeafd6d1863e950807029af84d5e11e0
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-02 14:21:35 +0400

    Gitlab client implementation

---
+++ Bot.Integration.Gitlab/GitlabClient.cs
@@ -0,0 +1,31 @@
+﻿using Bot.Core.Options;
+using Bot.Integration.Gitlab.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace Bot.Integration.Gitlab;
+
+public class GitlabClient : IGitlabClient
+{
+    private const string BASE_URL = "https://gitlab.com/api/v4";
+    private readonly HttpClient _httpClient;
+
+    public GitlabClient()
+    {
+        _httpClient = new HttpClient();
+        
+    }
+
+    public async Task SendAsync(IGitlabRequest request, CancellationToken cancellationToken)
+    {
+        
+        var requestMessage = new HttpRequestMessage
+        {
+            RequestUri = new Uri(BASE_URL+request.Url),
+            Method = request.Method,
+            Content = request.Content
+        };        
+        if (request.AccessToken != null)
+            requestMessage.Headers.Add("PRIVATE-TOKEN", request.AccessToken);
+        var response = await _httpClient.SendAsync(requestMessage, cancellationToken);
+    }
+}
