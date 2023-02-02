4bc50d2eb8a407c462584483f6481ee657c503c5
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-02 14:47:59 +0400

    Added logging

--- Bot.Integration.Gitlab/GitlabService.cs
+++ Bot.Integration.Gitlab/GitlabService.cs
@@ -3,6 +3,7 @@ using Bot.Core.Entities;
 using Bot.Core.Options;
 using Bot.Integration.Gitlab.Abstractions;
 using Bot.Integration.Gitlab.Requests;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Text.Json;
 
@@ -12,23 +13,31 @@ public class GitlabService : IGitlabService
 {    
     private readonly GitLabOptions _options;
     private readonly IGitlabClient _client;
+    private readonly ILogger<GitlabService> _logger;
 
-    public GitlabService(IGitlabClient client, GitLabOptions options)
+    public GitlabService(IGitlabClient client, GitLabOptions options, ILogger<GitlabService> logger)
     {
         _client = client;
         _options = options;
+        _logger = logger;
     }
 
-    public GitlabService(IGitlabClient client, IOptions<GitLabOptions> options) : this(client, options.Value)
+    public GitlabService(IGitlabClient client, IOptions<GitLabOptions> options, ILogger<GitlabService> logger)
+        : this(client, options.Value, logger)
     {
 
     }
 
     public async Task CommitFileAsync(CommitInfo file, CancellationToken cancellationToken = default)
     {
-          await _client.SendAsync(
+         var result = await _client.SendAsync(
               new CommitRequest(file.MapToRequest(_options), _options),
               cancellationToken
               );
+
+        if (result)
+            _logger.LogInformation($"Commited {file.FileName} from {file.From}");
+        else
+            _logger.LogCritical("Error during commiting file");        
     }    
 }
