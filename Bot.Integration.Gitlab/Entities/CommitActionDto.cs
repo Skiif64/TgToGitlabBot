61cab5e75606ddab005a889ed126f6704eee76df
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-02 14:19:47 +0400

    GitlabEntities

---
+++ Bot.Integration.Gitlab/Entities/CommitActionDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bot.Integration.Gitlab.Entities
+{
+    internal class CommitActionDto
+    {
+        [JsonPropertyName("action")]
+        public string Action { get; init; } = string.Empty;
+        [JsonPropertyName("file_path")]
+        public string FilePath { get; init; } = string.Empty;
+        [JsonPropertyName("content")]
+        public string Content { get; init; } = string.Empty;
+    }
+}
---
+++ Bot.Integration.Gitlab/Entities/CommitRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bot.Integration.Gitlab.Entities;
+
+internal class CommitRequestDto
+{    
+    [JsonPropertyName("branch")]
+    public string Branch { get; init; } = string.Empty;
+    [JsonPropertyName("commit_message")]
+    public string CommitMessage { get; init; } = string.Empty;
+    [JsonPropertyName("actions")]
+    public CommitActionDto[] Actions { get; init; } = Array.Empty<CommitActionDto>();
+}
