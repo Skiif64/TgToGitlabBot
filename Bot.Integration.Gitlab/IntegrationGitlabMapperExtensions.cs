c306b549d955f962b70f1b09d98d261388d01035
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-02 14:20:02 +0400

    MapperExtension

---
+++ Bot.Integration.Gitlab/IntegrationGitlabMapperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Bot.Core.Entities;
+using Bot.Core.Options;
+using Bot.Integration.Gitlab.Entities;
+
+namespace Bot.Integration.Gitlab;
+
+internal static class IntegrationGitlabMapperExtensions
+{
+    public static CommitRequestDto MapToRequest(this CommitInfo file, GitLabOptions options)
+    {
+        var request = new CommitRequestDto
+        {            
+            Branch = options.BranchName,
+            CommitMessage = file.Message,
+            Actions = new[]
+            {
+                new CommitActionDto
+                {
+                    Action = "create",
+                    FilePath = file.FileName,
+                    Content = file.Content
+                }
+            }
+        };
+        return request;
+    }
+}
