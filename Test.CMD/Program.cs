7c2d55b667aafec7c578634e58b521264e4951a3
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-02 14:21:16 +0400

    Revert "Test cmd"
    This reverts commit df3f196822b6af6e8c30e087e115a1c974a4f8e5.

--- Test.CMD/Program.cs
+++ Test.CMD/Program.cs
@@ -1,27 +1,2 @@
-﻿using Bot.Core.Entities;
-using Bot.Core.Options;
-using Bot.Integration.Gitlab;
-using Bot.Integration.Gitlab.Requests;
-
-var options = new GitLabOptions
-{
-    AuthorEmail = "Skiif64@yandex.ru",
-    AuthorUsername = "Skiif64",
-    BranchName = "main",
-    ProjectPath = "43107473",
-    ProjectAccesToken = "glpat-cfUSxsKzjSFk8ZWuu-tx"
-};
-
-var request = new CommitRequest(new CommitInfo
-{
-    FileId = "0",
-    Content = "example",
-    FileName = "example2.txt",
-    From = "Skiif64",
-    Message = "Example commit2"
-}.MapToRequest(options),
-options);
-
-var client = new GitlabClient();
-await client.SendAsync(request, default);
-Console.ReadLine();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
