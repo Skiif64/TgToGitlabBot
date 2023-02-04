dc2bd34e07614900e2258e782b219ec303f8ffcc
Skiif64 <skiif64@yandex.ru>
2023-02-04 16:25:46 +0400

    Changed text encoding to base64

--- Bot.Integration.Gitlab/Primitives/Base/CommitAction.cs
+++ Bot.Integration.Gitlab/Primitives/Base/CommitAction.cs
@@ -10,6 +10,28 @@ internal abstract class CommitAction
     public string FilePath { get; init; } = string.Empty;
     [JsonPropertyName("content")]
     public string? Content { get; init; }
+    [JsonPropertyName("encoding")]
+    public string? Encoding { get; init; } = "base64";
+
+    protected CommitAction(string filepath, string? content)
+    {
+        if (string.IsNullOrWhiteSpace(filepath))
+            throw new ArgumentNullException(nameof(filepath));
+        FilePath = filepath;
+        Content = content;
+    }
+
+    protected CommitAction(string filepath, Stream contentStream)
+    {
+        if (contentStream is null)
+            throw new ArgumentNullException(nameof(contentStream));
+        FilePath = filepath;
+        using (var br = new BinaryReader(contentStream))
+        {
+            contentStream.Position = 0;
+            Content = Convert.ToBase64String(br.ReadBytes((int)br.BaseStream.Length));
+        }
+    }
 
 
 }
