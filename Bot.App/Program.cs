75ac38229e44c8907b5202b7d49daf2c60792c74
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-07 14:51:48 +0400

    sligtly changes

--- Bot.App/Program.cs
+++ Bot.App/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory() + "/conf");
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.Configure<GitLabOptions>(builder.Configuration.GetRequiredSection(GitLabOptions.Path));
 builder.Services.Configure<TelegramBotOptions>(builder.Configuration.GetRequiredSection(TelegramBotOptions.Path));
 builder.Services.AddTransient<HttpClient>();
