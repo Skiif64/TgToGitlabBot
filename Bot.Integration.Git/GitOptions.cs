a98ecba4f3f58794c710efddab93f062114fcfd9
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-09 17:26:22 +0400

    Added basic implementation of repository

---
+++ Bot.Integration.Git/GitOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Bot.Integration.Git;
+
+internal class GitOptions
+{
+    public string Username { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public string AccessToken { get; init; } = string.Empty;
+    public string Branch { get; init; } = string.Empty;
+    public string Url { get; init; } = string.Empty;
+    public string LocalPath { get; init; } = string.Empty;
+}
---
+++ Bot.Integration.Git/GitRepository.cs
@@ -0,0 +1,63 @@
+﻿
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Test.CMD")]
+namespace Bot.Integration.Git;
+
+internal class GitRepository
+{	
+    private readonly GitOptions _options;
+    private readonly Identity _identity;
+    private CredentialsHandler _credentialsHandler;
+
+    public GitRepository(GitOptions options)
+    {
+        _options = options;
+        _identity = new Identity(_options.Username, _options.Email);
+        
+        _credentialsHandler = (url, user, type) => new UsernamePasswordCredentials
+        {
+            Username = _options.Username,
+            Password = _options.AccessToken
+        };
+    }
+
+    public void Commit(string message)
+    {
+        var signature = new Signature
+            (
+            _identity,
+            DateTimeOffset.UtcNow
+            );
+        using var repository = new Repository(_options.LocalPath);
+                
+        Commands.Stage(repository,"*"); //TODO: concrete file status
+        var commit = repository.Commit(message, signature, signature);
+       
+    }
+
+    public void Push()
+    {
+        using var repository = new Repository(_options.LocalPath);
+        var remote = repository.Network.Remotes["origin"];
+        
+        repository.Network.Push(remote, $@"refs/heads/{_options.Branch}", new PushOptions 
+        {
+            CredentialsProvider = _credentialsHandler            
+        });
+    }
+
+    public void Initialize()
+    {        
+        if (Repository.IsValid(_options.LocalPath))
+            return;
+
+        Repository.Clone(_options.Url, _options.LocalPath, new CloneOptions
+        {
+            BranchName = _options.Branch,
+            CredentialsProvider = _credentialsHandler
+        });
+    }
+}
