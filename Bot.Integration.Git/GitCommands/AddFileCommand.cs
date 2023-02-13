18bfd39ae1757abf147ca6a5bf1df95bf9412406
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-13 16:42:02 +0400

    Added buffer to writer

--- Bot.Integration.Git/GitCommands/AddFileCommand.cs
+++ Bot.Integration.Git/GitCommands/AddFileCommand.cs
@@ -4,29 +4,36 @@ using LibGit2Sharp;
 namespace Bot.Integration.Git.GitCommands;
 
 internal class AddFileCommand : IGitCommand
-{    
+{
     private readonly CommitInfo _commitInfo;
-    private readonly GitOptionsSection _optionsSection;    
+    private readonly GitOptionsSection _optionsSection;
 
     public AddFileCommand(CommitInfo commitInfo, GitOptionsSection optionsSection)
     {
         _commitInfo = commitInfo;
-        _optionsSection = optionsSection;       
+        _optionsSection = optionsSection;
     }
     public bool Execute(IRepository repository)
-    {       
+    {
         if (_commitInfo.Content is null)
             throw new ArgumentNullException(nameof(_commitInfo.Content));
         string filepath;
         if (_optionsSection.FilePath is not null)
             filepath = Path.Combine(_optionsSection.FilePath, _commitInfo.FileName);
         else
-            filepath = _commitInfo.FileName;        
+            filepath = _commitInfo.FileName;
         using (var fileStream = new FileStream(Path.Combine(_optionsSection.LocalPath, filepath), FileMode.OpenOrCreate, FileAccess.Write))
         {
-            using var writer = new BinaryWriter(fileStream);
-            using var reader = new BinaryReader(_commitInfo.Content);
-            writer.Write(reader.ReadBytes((int)_commitInfo.Content.Length));
+            using var reader = new BinaryReader(_commitInfo.Content);            
+            Span<byte> buffer = stackalloc byte[1024];
+            int readed;
+            while ((readed = reader.Read(buffer)) > 0)
+            {                
+                if (buffer.Length > readed)
+                    fileStream.Write(buffer[..readed]);
+                else
+                    fileStream.Write(buffer);
+            }
         }
         return true;
     }
