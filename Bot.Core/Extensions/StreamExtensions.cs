ddf2d4dc2151654650c5290b30f2ae39507f9ac5
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-20 10:29:00 +0400

    Added StreamExtensions

---
+++ Bot.Core/Extensions/StreamExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Bot.Core.Extensions;
+
+public static class StreamExtensions
+{
+    public static void WriteBinaryTo(this Stream source, Stream dest, int bufferSize = 1024)
+    {
+        Span<byte> buffer = stackalloc byte[bufferSize];
+        int readed;
+        while ((readed = source.Read(buffer)) > 0)
+        {
+            if (buffer.Length > readed)
+                dest.Write(buffer[..readed]);
+            else
+                dest.Write(buffer);
+        }
+    }
+}
