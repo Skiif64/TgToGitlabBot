c28a1a9ad46c5f8e7b5a26d6635afa803b91655e
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-20 12:00:13 +0400

    Fixed addFileTests for new logic

--- Bot.Integration.Git.Tests/AddFileCommandTests.cs
+++ Bot.Integration.Git.Tests/AddFileCommandTests.cs
@@ -1,7 +1,4 @@
-﻿using Bot.Core.Entities;
-using Bot.Integration.Git;
-using Bot.Integration.Git.GitCommands;
-using LibGit2Sharp;
+﻿using Bot.Integration.Git.GitCommands.AddFile;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -10,11 +7,10 @@ namespace Bot.Git.Tests;
 public class AddFileCommandTests
 {
     private const string REPOSITORY_PATH = "test-repository";
-
-
+    
     public AddFileCommandTests()
     {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);                
     }
 
     [SetUp]
@@ -28,17 +24,18 @@ public class AddFileCommandTests
     [TestCase("UTF-8-BIG.txt", "da51f821b7f49c35830356b68ab1bf50e63f9fcdd2f52fc981bfe318b2832232")]
     [TestCase("UTF-8.txt", "32158db1e9eb4a80c34f1f1ae11f5868ad80606c1a6e1ac4fbe95e6228da0c34")]
     [TestCase("WINDOWS-1251.txt", "35beb404565d5baab7bd964b9181ec3d9a730ec35320695b441eb6b5c889d7f4")]
-    public void WhenAddFile_ThenSHA256HashShouldBeEquals(string filename, string expectedHash)
+    public async Task WhenAddFile_ThenSHA256HashShouldBeEquals(string filename, string expectedHash)
     {
         var stream = new MemoryStream(GetFileBytes($"Fixtures/{filename}"));
-        var sha256 = SHA256.Create();  
-
+        var sha256 = SHA256.Create(); 
         var filepath = Path.Combine(REPOSITORY_PATH, filename);
+        var request = new AddFileCommand(stream, filepath);
+        var handler = new AddFileCommandHandler();
 
-        new AddFileCommand(stream, filepath).Execute(null!);        
-        var actualhash = sha256.ComputeHash(GetFileBytes(filepath));
-        var actualhashString = ByteArrayToHexString(actualhash);
+        await handler.Handle(request, default);
         
+        var actualhash = sha256.ComputeHash(GetFileBytes(filepath));
+        var actualhashString = ByteArrayToHexString(actualhash);        
         Assert.That(actualhashString, Is.EqualTo(expectedHash));
 
     }    
