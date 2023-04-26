34de4a3ca67e377566dda8c1f490d77645425f85
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-26 12:53:57 +0400

    Added Tests for formatters

---
+++ Bot.Integration.Telegram.Tests/MessageFormatTest.cs
@@ -0,0 +1,42 @@
+using Bot.Integration.Telegram.CommitFactories;
+using Moq;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bot.Integration.Telegram.Tests;
+
+public class MessageFormatTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase("AAAA_BBBBB_CCCCCC_v45_2022_66_02.7z", "AAAA_BBBBB_CCCCCC.7z")]
+    [TestCase("AAAA_BBBBB_CCCCCC_v45.zip", "AAAA_BBBBB_CCCCCC.zip")]
+    public async Task ChannelCommitRequestFactory_CreateCommitRequest_ThenFilenameShouldBeFormatted(string filename, string expectedFilename)
+    {
+        var client = new Mock<ITelegramBotClient>().Object;
+        var message = new Message
+        {
+            Chat = new Chat
+            {
+                Title = string.Empty
+            },
+            Document = new Document
+            {
+                FileName = filename,
+            }
+        };
+        var sutMock = new Mock<ChannelCommitRequestFactory>(new object[] { message, client });
+        sutMock.Setup(x => x.DownloadFileAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(Stream.Null));
+        sutMock.CallBase = true;
+        var sut = sutMock.Object;
+
+        var actual = await sut.CreateCommitRequestAsync(default);
+
+        Assert.That(actual, Is.Not.Null);
+        Assert.That(actual.FileName, Is.EqualTo(expectedFilename));
+    }
+}
