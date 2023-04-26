de7b126b63710b8cb6ccb903d77c9624581c89fb
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-26 12:30:59 +0400

    Added formatters

---
+++ Bot.Integration.Telegram/Formatters/VersionDateShrinkFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace Bot.Integration.Telegram.Formatters;
+internal class VersionDateShrinkFormatter : IFormatter
+{
+    private const string Pattern = @"_v\d+(_\d+)*.+$";    
+    public string Format(string message)
+    {
+        var extension = Path.GetExtension(message);
+        var filename = Path.GetFileNameWithoutExtension(message);
+        var result = Regex.Replace(filename, Pattern, "");
+        return result + extension;
+    }
+}
